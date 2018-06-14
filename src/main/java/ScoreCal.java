fc073b67671ee160a6fe1fd1fa5c02d0e2b79f2b
ASUS\USER <jiyeon930607a@gmail.com>
2018-06-14 14:16:51 +0800

    完成

--- src/main/java/ScoreCal.java
+++ src/main/java/ScoreCal.java
@@ -8,29 +8,121 @@ public class ScoreCal {
 		int fin = list[3];
 		int quiz = list[4];
 		int att = list[5];
+		idtest(list[0]);
+		gpatest(list[1]); 
+		midtest(list[2]);
+		fintest(list[3]);
+		quiztest(list[4]);
+		atttest(list[5]);
 		String result = "";
 		String attr = "";
-		assert(gpa >= 0 && gpa <= 100);
-		assert(mid >= 0 && mid <= 100);
-		assert(fin >= 0 && fin <= 100);
-		assert(quiz >= 0 && quiz <= 100);
-		assert(att == 0 || att == 1);
-		assert(id != 0);
 		double sum01;
-		System.out.println("\n學號\t平時\t期中\t期末\t小考\t全勤\t總分\t等級"); 
+		System.out.println("\n學號\t平時\t期中\t期末\t小考\t總分"); 
 		sum01=(gpa* 0.2) + (mid* 0.3) + (fin* 0.3) + (quiz* 0.2) ; 
 		int sum = (int)sum01;
+		assert(sum <= 100);
+		leveltest(sum);
 		if (att == 0) {
-			attr = "否";
+			attrtest(att);
 		}
 		if(att == 1) {
 			sum = sum + 5;
-			attr = "是";
+			attrtest(att);
 			if(sum > 100) {
 				sum = 100;
 			}
 		}
-		assert(sum <= 100);
+		System.out.println(id +"\t" + gpa + "\t" + mid +"\t" + fin +"\t" + quiz +"\t" + sum); 
+		System.out.println("_____________________________________________________"); 
+		
+		return sum ;
+	}
+	public int idtest(int id) {
+		try
+		{
+		  if(id <= 0 || id >= 10000)
+		  throw new ArithmeticException();
+		  else
+		  return id;
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		return id;
+	}
+	public int gpatest(int gpa) {
+		try
+		{
+		  if(gpa < 0 || gpa > 100)
+		  throw new ArithmeticException();
+		  else
+		  return gpa;
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		return gpa;
+	}
+	public int midtest(int mid) {
+		try
+		{
+		  if(mid < 0 || mid > 100)
+		  throw new ArithmeticException();
+		  else
+		  return mid;
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		return mid;
+	}
+	public int fintest(int fin) {
+		try
+		{
+		  if(fin < 0 || fin > 100)
+		  throw new ArithmeticException();
+		  else
+		  return fin;
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		return fin;
+	}
+	public int quiztest(int quiz) {
+		try
+		{
+		  if(quiz < 0 || quiz > 100)
+		  throw new ArithmeticException();
+		  else
+		  return quiz;
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		return quiz;
+	}
+	public int atttest(int att) {
+		try
+		{
+		  if(att < 0 || att > 1)
+		  throw new ArithmeticException();
+		  else
+		  return att;
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		return att;
+	}
+	public String leveltest(int sum) {
+		String result = "";
 		if(sum >= 0 && sum < 60) {
 			result = "F";		
 		}
@@ -49,10 +141,27 @@ public class ScoreCal {
 		if(sum == 100) {
 			result = "S";		
 		}
-		System.out.println(id +"\t" + gpa + "\t" + mid +"\t" + fin +"\t" + quiz +"\t" + attr +"\t" + sum +"\t" + result); 
-		System.out.println("_____________________________________________________"); 
-		
-		return sum ;
-		
+		return result;
 	}
+	public String attrtest(int att) {
+		String attr = "";
+		try
+		{
+		  if(att < 0 || att > 1)
+		  throw new ArithmeticException();
+		}
+		catch(ArithmeticException e)
+		{
+		  System.out.println(e+" 被拋出");
+		}
+		if(att == 0) {
+			attr = "否";
+		}
+		if(att == 1) {
+			attr = "是";
+		}
+		
+		return attr;
+	}
+
 }
--- src/test/java/ScoreCalTest.java
+++ src/test/java/ScoreCalTest.java
@@ -9,52 +9,192 @@ public class ScoreCalTest {
 	private ScoreCal a = new ScoreCal();
 
 	@Test
-	public void test() {
-		assertEquals(0, a.Score(new int[] {123,0,0,0,0,0}));
-		assertEquals(60, a.Score(new int[] {456,100,50,50,50,0}));
-		assertEquals(19, a.Score(new int[] {147,99,0,0,0,0}));
-//		一項成績=0
-		assertEquals(0, a.Score(new int[] {123,0,0,0,0,0}));
+	public void testall() {
+
+//		一項成績=0,全情0或1
 		assertEquals(40, a.Score(new int[] {123,0,50,50,50,0}));
 		assertEquals(35, a.Score(new int[] {123,50,0,50,50,0}));
 		assertEquals(35, a.Score(new int[] {123,50,50,0,50,0}));
 		assertEquals(40, a.Score(new int[] {123,50,50,50,0,0}));
-//		一項成績=100	
-		assertEquals(60, a.Score(new int[] {456,100,50,50,50,0}));
-		assertEquals(65, a.Score(new int[] {456,50,100,50,50,0}));
-		assertEquals(65, a.Score(new int[] {456,50,50,100,50,0}));
-		assertEquals(60, a.Score(new int[] {456,50,50,50,100,0}));
-//		全勤=1
-		assertEquals(55, a.Score(new int[] {456,50,50,50,50,1}));
-//		一項成績=1
-		assertEquals(0, a.Score(new int[] {789,1,0,0,0,0}));
-		assertEquals(0, a.Score(new int[] {789,0,1,0,0,0}));
-		assertEquals(0, a.Score(new int[] {789,0,0,1,0,0}));
-		assertEquals(0, a.Score(new int[] {789,0,0,0,1,0}));
-//		一項成績=99
-		assertEquals(19, a.Score(new int[] {147,99,0,0,0,0}));
-		assertEquals(29, a.Score(new int[] {147,0,99,0,0,0}));
-		assertEquals(29, a.Score(new int[] {147,0,0,99,0,0}));
-		assertEquals(19, a.Score(new int[] {147,0,0,0,99,0}));
-//		一項成績=50
-		assertEquals(10, a.Score(new int[] {258,50,0,0,0,0}));
-		assertEquals(15, a.Score(new int[] {258,0,50,0,0,0}));
-		assertEquals(15, a.Score(new int[] {258,0,0,50,0,0}));
-		assertEquals(10, a.Score(new int[] {258,0,0,0,50,0}));
+		
+		assertEquals(45, a.Score(new int[] {123,0,50,50,50,1}));
+		assertEquals(40, a.Score(new int[] {123,50,0,50,50,1}));
+		assertEquals(40, a.Score(new int[] {123,50,50,0,50,1}));
+		assertEquals(45, a.Score(new int[] {123,50,50,50,0,1}));
+//		兩項成績=0,全情0或1
+		assertEquals(25, a.Score(new int[] {123,0,0,50,50,0}));
+		assertEquals(25, a.Score(new int[] {123,0,50,0,50,0}));
+		assertEquals(30, a.Score(new int[] {123,0,50,50,0,0}));
+		assertEquals(20, a.Score(new int[] {123,50,0,0,50,0}));
+		assertEquals(25, a.Score(new int[] {123,50,0,50,0,0}));
+		assertEquals(25, a.Score(new int[] {123,50,50,0,0,0}));
+		
+		assertEquals(30, a.Score(new int[] {123,0,0,50,50,1}));
+		assertEquals(30, a.Score(new int[] {123,0,50,0,50,1}));
+		assertEquals(35, a.Score(new int[] {123,0,50,50,0,1}));
+		assertEquals(25, a.Score(new int[] {123,50,0,0,50,1}));
+		assertEquals(30, a.Score(new int[] {123,50,0,50,0,1}));
+		assertEquals(30, a.Score(new int[] {123,50,50,0,0,1}));
+//		三項成績=0,全情0或1
+		assertEquals(10, a.Score(new int[] {123,0,0,0,50,0}));
+		assertEquals(15, a.Score(new int[] {123,0,0,50,0,0}));
+		assertEquals(15, a.Score(new int[] {123,0,50,0,0,0}));
+		assertEquals(10, a.Score(new int[] {123,50,0,0,0,0}));
+		
+		assertEquals(15, a.Score(new int[] {123,0,0,0,50,1}));
+		assertEquals(20, a.Score(new int[] {123,0,0,50,0,1}));
+		assertEquals(20, a.Score(new int[] {123,0,50,0,0,1}));
+		assertEquals(15, a.Score(new int[] {123,50,0,0,0,1}));
+//		全部成績為0,全情0或1
+		assertEquals(0, a.Score(new int[] {123,0,0,0,0,0}));
+		assertEquals(5, a.Score(new int[] {123,0,0,0,0,1}));
+//		一項成績=100,全情0或1	
+		assertEquals(60, a.Score(new int[] {123,100,50,50,50,0}));
+		assertEquals(65, a.Score(new int[] {123,50,100,50,50,0}));
+		assertEquals(65, a.Score(new int[] {123,50,50,100,50,0}));
+		assertEquals(60, a.Score(new int[] {123,50,50,50,100,0}));
+		
+		assertEquals(65, a.Score(new int[] {123,100,50,50,50,1}));
+		assertEquals(70, a.Score(new int[] {123,50,100,50,50,1}));
+		assertEquals(70, a.Score(new int[] {123,50,50,100,50,1}));
+		assertEquals(65, a.Score(new int[] {123,50,50,50,100,1}));
+//		兩項成績=100,全情0或1	
+		assertEquals(75, a.Score(new int[] {123,100,100,50,50,0}));
+		assertEquals(75, a.Score(new int[] {123,100,50,100,50,0}));
+		assertEquals(70, a.Score(new int[] {123,100,50,50,100,0}));
+		assertEquals(80, a.Score(new int[] {123,50,100,100,50,0}));
+		assertEquals(75, a.Score(new int[] {123,50,100,50,100,0}));
+		assertEquals(75, a.Score(new int[] {123,50,50,100,100,0}));
+		
+		assertEquals(80, a.Score(new int[] {123,100,100,50,50,1}));
+		assertEquals(80, a.Score(new int[] {123,100,50,100,50,1}));
+		assertEquals(75, a.Score(new int[] {123,100,50,50,100,1}));
+		assertEquals(85, a.Score(new int[] {123,50,100,100,50,1}));
+		assertEquals(80, a.Score(new int[] {123,50,100,50,100,1}));
+		assertEquals(80, a.Score(new int[] {123,50,50,100,100,1}));
+//		三項成績=100,全情0或1	
+		assertEquals(90, a.Score(new int[] {123,100,100,100,50,0}));
+		assertEquals(85, a.Score(new int[] {123,100,100,50,100,0}));
+		assertEquals(85, a.Score(new int[] {123,100,50,100,100,0}));
+		assertEquals(90, a.Score(new int[] {123,50,100,100,100,0}));
+		
+		assertEquals(95, a.Score(new int[] {123,100,100,100,50,1}));
+		assertEquals(90, a.Score(new int[] {123,100,100,50,100,1}));
+		assertEquals(90, a.Score(new int[] {123,100,50,100,100,1}));
+		assertEquals(95, a.Score(new int[] {123,50,100,100,100,1}));
+//		全部一百，全勤0或1
+		assertEquals(100, a.Score(new int[] {123,100,100,100,100,0}));
+		
+		assertEquals(100, a.Score(new int[] {123,100,100,100,100,1}));
+//		全勤=0或1
+		assertEquals(50, a.Score(new int[] {123,50,50,50,50,0}));
+		assertEquals(55, a.Score(new int[] {123,50,50,50,50,1}));
+//		一項成績=1，全勤0或1
+		assertEquals(40, a.Score(new int[] {123,1,50,50,50,0}));
+		assertEquals(35, a.Score(new int[] {123,50,1,50,50,0}));
+		assertEquals(35, a.Score(new int[] {123,50,50,1,50,0}));
+		assertEquals(40, a.Score(new int[] {123,50,50,50,1,0}));
+		
+		assertEquals(45, a.Score(new int[] {123,1,50,50,50,1}));
+		assertEquals(40, a.Score(new int[] {123,50,1,50,50,1}));
+		assertEquals(40, a.Score(new int[] {123,50,50,1,50,1}));
+		assertEquals(45, a.Score(new int[] {123,50,50,50,1,1}));
+//		兩項成績=1，全勤0或1
+		assertEquals(25, a.Score(new int[] {123,1,1,50,50,0}));
+		assertEquals(25, a.Score(new int[] {123,1,50,1,50,0}));
+		assertEquals(30, a.Score(new int[] {123,1,50,50,1,0}));
+		assertEquals(20, a.Score(new int[] {123,50,1,1,50,0}));
+		assertEquals(25, a.Score(new int[] {123,50,1,50,1,0}));
+		assertEquals(25, a.Score(new int[] {123,50,50,1,1,0}));
+		
+		assertEquals(30, a.Score(new int[] {123,1,1,50,50,1}));
+		assertEquals(30, a.Score(new int[] {123,1,50,1,50,1}));
+		assertEquals(35, a.Score(new int[] {123,1,50,50,1,1}));
+		assertEquals(25, a.Score(new int[] {123,50,1,1,50,1}));
+		assertEquals(30, a.Score(new int[] {123,50,1,50,1,1}));
+		assertEquals(30, a.Score(new int[] {123,50,50,1,1,1}));
+//		三項成績=1，全勤0或1
+		assertEquals(10, a.Score(new int[] {123,1,1,1,50,0}));
+		assertEquals(15, a.Score(new int[] {123,1,1,50,1,0}));
+		assertEquals(15, a.Score(new int[] {123,1,50,1,1,0}));
+		assertEquals(10, a.Score(new int[] {123,50,1,1,1,0}));
+		
+		assertEquals(15, a.Score(new int[] {123,1,1,1,50,1}));
+		assertEquals(20, a.Score(new int[] {123,1,1,50,1,1}));
+		assertEquals(20, a.Score(new int[] {123,1,50,1,1,1}));
+		assertEquals(15, a.Score(new int[] {123,50,1,1,1,1}));
+//		全部成績為1，全勤0或1
+		assertEquals(1, a.Score(new int[] {123,1,1,1,1,0}));
+		
+		assertEquals(6, a.Score(new int[] {123,1,1,1,1,1}));
+//		一項成績=99，全勤0或1
+		assertEquals(59, a.Score(new int[] {123,99,50,50,50,0}));
+		assertEquals(64, a.Score(new int[] {123,50,99,50,50,0}));
+		assertEquals(64, a.Score(new int[] {123,50,50,99,50,0}));
+		assertEquals(59, a.Score(new int[] {123,50,50,50,99,0}));
+		
+		assertEquals(64, a.Score(new int[] {123,99,50,50,50,1}));
+		assertEquals(69, a.Score(new int[] {123,50,99,50,50,1}));
+		assertEquals(69, a.Score(new int[] {123,50,50,99,50,1}));
+		assertEquals(64, a.Score(new int[] {123,50,50,50,99,1}));
+//		兩項成績=99，全勤0或1
+		assertEquals(74, a.Score(new int[] {123,99,99,50,50,0}));
+		assertEquals(74, a.Score(new int[] {123,99,50,99,50,0}));
+		assertEquals(69, a.Score(new int[] {123,99,50,50,99,0}));
+		assertEquals(79, a.Score(new int[] {123,50,99,99,50,0}));
+		assertEquals(74, a.Score(new int[] {123,50,99,50,99,0}));
+		assertEquals(74, a.Score(new int[] {123,50,50,99,99,0}));
+		
+		assertEquals(79, a.Score(new int[] {123,99,99,50,50,1}));
+		assertEquals(79, a.Score(new int[] {123,99,50,99,50,1}));
+		assertEquals(74, a.Score(new int[] {123,99,50,50,99,1}));
+		assertEquals(84, a.Score(new int[] {123,50,99,99,50,1}));
+		assertEquals(79, a.Score(new int[] {123,50,99,50,99,1}));
+		assertEquals(79, a.Score(new int[] {123,50,50,99,99,1}));
+
+//		三項成績=99，全勤0或1
+		assertEquals(89, a.Score(new int[] {123,99,99,99,50,0}));
+		assertEquals(84, a.Score(new int[] {123,99,99,50,99,0}));
+		assertEquals(84, a.Score(new int[] {123,99,50,99,99,0}));
+		assertEquals(89, a.Score(new int[] {123,50,99,99,99,0}));
+		
+		assertEquals(94, a.Score(new int[] {123,99,99,99,50,1}));
+		assertEquals(89, a.Score(new int[] {123,99,99,50,99,1}));
+		assertEquals(89, a.Score(new int[] {123,99,50,99,99,1}));
+		assertEquals(94, a.Score(new int[] {123,50,99,99,99,1}));
+
+//		全部成績=99，全勤0或1
+		assertEquals(99, a.Score(new int[] {123,99,99,99,99,0}));
+		
+		assertEquals(100, a.Score(new int[] {123,99,99,99,99,1}));
+//		一項成績=50，全勤0或1
+		assertEquals(10, a.Score(new int[] {123,50,0,0,0,0}));
+		assertEquals(15, a.Score(new int[] {123,0,50,0,0,0}));
+		assertEquals(15, a.Score(new int[] {123,0,0,50,0,0}));
+		assertEquals(10, a.Score(new int[] {123,0,0,0,50,0}));
+		
+		assertEquals(15, a.Score(new int[] {123,50,0,0,0,1}));
+		assertEquals(20, a.Score(new int[] {123,0,50,0,0,1}));
+		assertEquals(20, a.Score(new int[] {123,0,0,50,0,1}));
+		assertEquals(15, a.Score(new int[] {123,0,0,0,50,1}));
 //		加權 >100 
-		assertEquals(100, a.Score(new int[] {321,100,100,100,90,1}));
+		assertEquals(100, a.Score(new int[] {123,100,100,100,100,1}));
 //		等級F
-		assertEquals(44, a.Score(new int[] {654,30,25,60,40,1}));
+		assertEquals(44, a.Score(new int[] {123,30,25,60,40,1}));
 //		等級D
-		assertEquals(61, a.Score(new int[] {987,60,70,50,65,0}));
+		assertEquals(61, a.Score(new int[] {123,60,70,50,65,0}));
 //		等級C
-		assertEquals(73, a.Score(new int[] {741,60,80,60,70,1}));
+		assertEquals(73, a.Score(new int[] {123,60,80,60,70,1}));
 //		等級B
-		assertEquals(81, a.Score(new int[] {852,80,80,85,82,0}));
+		assertEquals(81, a.Score(new int[] {123,80,80,85,82,0}));
 //		等級A
-		assertEquals(96, a.Score(new int[] {963,92,90,95,88,1}));
+		assertEquals(96, a.Score(new int[] {123,92,90,95,88,1}));
 //		等級S
-		assertEquals(100, a.Score(new int[] {111,100,100,100,100,0}));
+		assertEquals(100, a.Score(new int[] {123,100,100,100,100,0}));
+//		全勤超過範圍
+		assertEquals(100, a.Score(new int[] {123,100,100,100,100,2}));
+		
+		assertEquals(100, a.Score(new int[] {123,100,100,100,100,-1}));
 		
 ////	一項成績=-1
 //	assertEquals(0, a.Score(new int[] {369,-1,0,0,0,0}));
@@ -68,10 +208,122 @@ public class ScoreCalTest {
 //	assertEquals(0, a.Score(new int[] {369,0,101,0,0,0}));
 //	assertEquals(0, a.Score(new int[] {369,0,0,101,0,0}));
 //	assertEquals(0, a.Score(new int[] {369,0,0,0,101,0}));
-////	全勤=2
-//	assertEquals(0, a.Score(new int[] {369,0,0,0,0,2}));
+////	ID=10000
+//	assertEquals(0, a.Score(new int[] {10000,90,80,70,60,0}));
 ////	ID=0
-//	assertEquals(80, a.Score(new int[] {0,90,80,70,60,1}));
+//	assertEquals(80, a.Score(new int[] {0	));
+//		
+	}
+	@Test
+	public void testid() { 
+		assertEquals(0,a.idtest(0));
+		assertEquals(1,a.idtest(1));
+		assertEquals(2,a.idtest(2));
+		assertEquals(9998,a.idtest(9998));
+		assertEquals(9999,a.idtest(9999));
+		assertEquals(10000,a.idtest(10000));
+		assertEquals(500,a.idtest(500));
+
+	}
+	public void testgpa() { 
+		assertEquals(-1,a.gpatest(-1));
+		assertEquals(0,a.gpatest(0));
+		assertEquals(1,a.gpatest(1));
+		assertEquals(99,a.gpatest(99));
+		assertEquals(100,a.gpatest(100));
+		assertEquals(101,a.gpatest(101));
+		assertEquals(50,a.gpatest(50));
 		
 	}
+	public void testmid() { 
+		assertEquals(-1,a.midtest(-1));
+		assertEquals(0,a.midtest(0));
+		assertEquals(1,a.midtest(1));
+		assertEquals(99,a.midtest(99));
+		assertEquals(100,a.midtest(100));
+		assertEquals(101,a.midtest(101));
+		assertEquals(50,a.midtest(50));
+		
+	}
+	public void testfin() { 
+		assertEquals(-1,a.fintest(-1));
+		assertEquals(0,a.fintest(0));
+		assertEquals(1,a.fintest(1));
+		assertEquals(99,a.fintest(99));
+		assertEquals(100,a.fintest(100));
+		assertEquals(101,a.fintest(101));
+		assertEquals(50,a.fintest(50));
+		
+	}
+	public void testquiz() { 
+		assertEquals(-1,a.quiztest(-1));
+		assertEquals(0,a.quiztest(0));
+		assertEquals(1,a.quiztest(1));
+		assertEquals(99,a.quiztest(99));
+		assertEquals(100,a.quiztest(100));
+		assertEquals(101,a.quiztest(101));
+		assertEquals(50,a.quiztest(50));
+		
+	}
+	public void testatt() { 
+		assertEquals(-1,a.atttest(-1));
+		assertEquals(0,a.atttest(0));
+		assertEquals(1,a.atttest(1));
+		assertEquals(2,a.atttest(2));
+		
+	}
+	
+	public void testattr() { 
+		assertEquals("",a.attrtest(-1));
+		assertEquals("否",a.attrtest(0));
+		assertEquals("是",a.attrtest(1));
+		assertEquals("",a.attrtest(2));
+	}
+	
+	public void testlevel() { 
+		assertEquals("F",a.leveltest(0));
+		assertEquals("F",a.leveltest(59));
+		assertEquals("F",a.leveltest(1));
+		assertEquals("F",a.leveltest(58));
+		assertEquals("F",a.leveltest(30));
+		assertEquals("",a.leveltest(-1));
+		assertEquals("D",a.leveltest(60));
+		
+		assertEquals("D",a.leveltest(60));
+		assertEquals("D",a.leveltest(69));
+		assertEquals("D",a.leveltest(61));
+		assertEquals("D",a.leveltest(68));
+		assertEquals("D",a.leveltest(65));
+		assertEquals("F",a.leveltest(59));
+		assertEquals("C",a.leveltest(70));
+		
+		assertEquals("C",a.leveltest(70));
+		assertEquals("C",a.leveltest(79));
+		assertEquals("C",a.leveltest(71));
+		assertEquals("C",a.leveltest(78));
+		assertEquals("C",a.leveltest(75));
+		assertEquals("D",a.leveltest(69));
+		assertEquals("B",a.leveltest(80));
+		
+		assertEquals("B",a.leveltest(80));
+		assertEquals("B",a.leveltest(89));
+		assertEquals("B",a.leveltest(81));
+		assertEquals("B",a.leveltest(88));
+		assertEquals("B",a.leveltest(85));
+		assertEquals("C",a.leveltest(79));
+		assertEquals("A",a.leveltest(90));
+		
+		assertEquals("A",a.leveltest(90));
+		assertEquals("A",a.leveltest(99));
+		assertEquals("A",a.leveltest(91));
+		assertEquals("A",a.leveltest(98));
+		assertEquals("A",a.leveltest(95));
+		assertEquals("B",a.leveltest(89));
+		assertEquals("S",a.leveltest(100));
+		
+		assertEquals("S",a.leveltest(100));
+		assertEquals("A",a.leveltest(99));
+		assertEquals("",a.leveltest(101));
+	}
+	
 }
